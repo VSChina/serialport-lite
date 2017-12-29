e4cb1c2f2138671276096c00b711b0b2b464435f
Li Zhe <lizhe@lizhe.org>
2017-12-30 01:27:30 +0800

    save

--- src/test.ts
+++ src/test.ts
@@ -5,8 +5,7 @@ try {
     console.log(list);
   });
 
-  SerialPortLite.write(
-      '/dev/ttyACM0', 'set_wifissid sneezry\r', 115200);
+  SerialPortLite.write('/dev/ttyACM0', 'set_wifissid sneezry\r', 115200);
 } catch (e) {
   console.log(e);
 }
