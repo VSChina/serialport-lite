b2a12364b9886cacaec96e1367721681425520b9
Zhe Li <zlhe@microsoft.com>
2017-12-29 12:14:17 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import * as os from 'os';
 import {exec} from 'child_process';
 
 interface DeviceInfo {
-    path: string,
-    vendorId: number
-    productId: number
+    port: string;
+    vendorId: number;
+    productId: number;
 }
 
 export class SerialPortLite {
@@ -20,7 +20,7 @@ export class SerialPortLite {
                     resolve(list);
                 });
             } else if (os.type() === 'Darwin') {
-                exec('system_profiler SPUSBDataType', (error, stdout, stderr) => {
+                exec('ls -l /dev/tty.*', (error, stdout, stderr) => {
                     if (error) {
                         reject(error);
                         return;
@@ -29,9 +29,52 @@ export class SerialPortLite {
                     resolve(list);
                 });
             } else if (os.type() === 'Linux') {
-                resolve([]);
+                exec('ls -l /dev/tty.*', (error, stdout, stderr) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    let list = parseUnixCOMList(stdout);
+                    resolve(list);
+                });
             } else {
                 reject(new Error(`Unsupported OS: ${os.type()}`));
+            }
+        });
+    }
+
+    static async write(port: string, data: string, speed: number): Promise<boolean> {
+        return new Promise(async (resolve: (value: boolean) => void, reject: (resaon: Error) => void) => {
+            if (os.type() === 'Windows_NT') {
+                exec(`mode ${port}:${speed},n,8,1`, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    exec(`echo ${data} > \\\\.\\${port}`, (error, stdout, stderr) => {
+                        if (error) {
+                            reject(error);
+                            return;
+                        }
+
+                        resolve(true);
+                    });
+                });
+            } else {
+                exec(`stty -speed ${speed} < ${port}`, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    exec(`echo -ne '${data}' > ${port}`, (error, stdout, stderr) => {
+                        if (error) {
+                            reject(error);
+                            return;
+                        }
+
+                        resolve(true);
+                    });
+                });
             }
         });
     }
@@ -42,16 +85,15 @@ function parseWindowsCOMList(rawList: string): DeviceInfo[] {
     let list: DeviceInfo[] = [];
     for (var i = 1; i < rows.length; i++) {
         let row = rows[i].split(/\s{2,}/);
-        let path = row[7];
+        let port = row[7];
         let ids = row[13];
-        console.log(ids)
         let vidMatches = ids.match(/VID_([A-F0-9]+)&/);
         let pidMatches = ids.match(/PID_([A-F0-9]+)&/);
         let vid = vidMatches && vidMatches.length > 1 ? vidMatches[1] : null;
         let pid = pidMatches && pidMatches.length > 1 ? pidMatches[1] : null;
 
         list.push({
-            path: row[7],
+            port: row[7],
             vendorId: vid ? parseInt(vid, 16) : null,
             productId: pid ? parseInt(pid, 16) : null,
         })
@@ -62,6 +104,13 @@ function parseWindowsCOMList(rawList: string): DeviceInfo[] {
 
 function parseUnixCOMList(rawList: string): DeviceInfo[] {
     let list: DeviceInfo[] = [];
-    let lines = rawList.split('\n');
+    let lines = rawList.trim().split('\n');
+    lines.forEach(line => {
+        list.push({
+            port: line,
+            vendorId: null,
+            productId: null
+        })
+    })
     return list;
 }
